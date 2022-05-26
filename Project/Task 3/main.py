5ff795878d0e98638201360f265250eb79a19802
IUTA <vasavaseckin801@gmail.com>
2022-05-27 00:06:52 +0300

    Task 3

--- Project/Task 3/main.py
+++ Project/Task 3/main.py
@@ -7,16 +7,49 @@ print("Опция 5: 6 числа")
 option = int(input("Выберите опцию для проведения операции: "))
 
 if option == 1:
+
     print("Опция '1': 2 числа")
+    number_1 = float(input("Введите первое число: "))
+    number_2 = float(input("Введите второе число: "))
+
+    print("")
+
 elif option == 2:
+
     print("Опция '2': 3 числа")
+    number_1 = float(input("Введите первое число: "))
+    number_2 = float(input("Введите второе число: "))
+    number_3 = float(input("Введите третье число: "))
+
 elif option == 3:
+
     print("Опция '3': 4 числа")
+    number_1 = float(input("Введите первое число: "))
+    number_2 = float(input("Введите второе число: "))
+    number_3 = float(input("Введите третье число: "))
+    number_4 = float(input("Введите четвёртое число: "))
+
 elif option == 4:
+
     print("Опция '4': 5 чисел")
+    number_1 = float(input("Введите первое число: "))
+    number_2 = float(input("Введите второе число: "))
+    number_3 = float(input("Введите третье число: "))
+    number_4 = float(input("Введите четвёртое число: "))
+    number_5 = float(input("Введите пятое число: "))
+
 elif option == 5:
+
     print("Опция '5': 6 чисел")
+    number_1 = float(input("Введите первое число: "))
+    number_2 = float(input("Введите второе число: "))
+    number_3 = float(input("Введите третье число: "))
+    number_4 = float(input("Введите четвёртое число: "))
+    number_5 = float(input("Введите пятое число: "))
+    number_6 = float(input("Введите шестое число: "))
+
 else:
+
     print("Вы ввели число не из диапазона или ввели текст")
     print("Завершите программу и введите правильное значение")
 
